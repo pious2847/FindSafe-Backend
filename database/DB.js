adc81a0a30a9e8d85cb251194da7adb0003df4be
pious <109950629+pious2847@users.noreply.github.com>
2024-05-02 11:40:25 +0000

    Changes to be committed: 	modified:   database/DB.js

--- database/DB.js
+++ database/DB.js
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 
 const ConnectDB =  ()=>{
+  const atlasConnectionUri = process.env.DBConnectionLink || 'mongodb+srv://abdulhafis2847:pious2847@findsafe.qgtvkt9.mongodb.net/'
+
     try {
-     mongoose.connect(process.env.DBConnectionLink, {
+     mongoose.connect( atlasConnectionUri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
           });
           console.log("---- Database connection successfully ----");
+          return mongoose.connection;
     } catch (error) {
         console.log("---- Erro in Database connection  ----", error);
     }
