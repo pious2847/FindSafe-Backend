22163db5a61e4dea1f7d27f3c4de2e503d5becfb
pious <109950629+pious2847@users.noreply.github.com>
2024-05-04 13:58:02 +0000

    Changes to be committed: 	modified:   Routes/user.js 	modified:   node_modules/.package-lock.json

--- Routes/user.js
+++ Routes/user.js
@@ -31,13 +31,14 @@ router.post('/api/signup', async (req, res) => {
        password: hashedPassword
       });
   // send verified codes for user
-      await sendResetEmail(email, user, req, res);
+      const emailsend = await sendResetEmail(email, user, req, res);
 
-      // Save the new user to the database
+      if (emailsend) {
+        // Save the new user to the database
       await user.save();
-
       res.status(200).send('Account created successfully,' + 'Verification Code Sent Successfully');
-
+      }
+      
     } catch (error) {
       res.status(500).send('An error occurred: ' + error.message);
     }
--- utils/MailSender.js
+++ utils/MailSender.js
@@ -3,12 +3,13 @@ import bcrypt from "bcrypt";
 import PasswordReset from "../models/utils_models/PasswordReset.js";
 import Users from "../models/users.js";
 
-let transporter = nodemailer.createTransport({
+const transporter =  nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
+  port: 465,
   secure: true, // true for 465, false for other ports
   service: "gmail",
-  auth: { user: process.env.AUTH_EMAIL, pass: process.env.AUTH_PASS },
+  auth: { user: process.env.AUTH_EMAIL, 
+    pass: process.env.AUTH_PASS }
 });
 
 
@@ -18,46 +19,53 @@ const sendResetEmail = async (email, user,  _req, res) => {
   }
 
   const verificationCode = `${Math.floor(100000 + Math.random() * 900000)}`;
+ await transporter.verify((error, success)=>{
+   if (error) {
+    console.log('===============================================', error);
+   }else{
+    console.log( success);
+   }
+  })
 
-  const mailOptions = {
-    from: process.env.AUTH_EMAIL,
-    to: email,
-    subject: 'Password Verification Code',
-    html: `
-      <div style="background-color: #f0f0f0; padding: 20px;">
-        <h2 style="color: #333; font-size: 24px;">Password Verification Code</h2>
-        <p style="color: #555; font-size: 16px;">Enter the verification code below to verify your email and initiate a password reset.</p>
-        <p style="color: #555; font-size: 16px;"><b>Verification Code:</b> <span style="background-color: #e0e0e0; padding: 5px 10px; border-radius: 5px;">${verificationCode}</span></p>
-        <p style="color: #555; font-size: 16px;">This code expires in 1 hour.</p>
-      </div>
-    `,
-  };
+  // const mailOptions = {
+  //   from: process.env.AUTH_EMAIL,
+  //   to: email,
+  //   subject: 'Password Verification Code',
+  //   html : `
+  //     <div style="background-color: #f0f0f0; padding: 20px;">
+  //       <h2 style="color: #333; font-size: 24px;">Password Verification Code</h2>
+  //       <p style="color: #555; font-size: 16px;">Enter the verification code below to verify your email and initiate a password reset.</p>
+  //       <p style="color: #555; font-size: 16px;"><b>Verification Code:</b> <span style="background-color: #e0e0e0; padding: 5px 10px; border-radius: 5px;">${verificationCode}</span></p>
+  //       <p style="color: #555; font-size: 16px;">This code expires in 1 hour.</p>
+  //     </div>
+  //   `,
+  // };
 
-  try {
-    await PasswordReset.deleteMany({ userId: user._id });
-    const salt = 10;
-    const hashedVerificationCode = await bcrypt.hash(verificationCode, salt);
+  // try {
+  //   await PasswordReset.deleteMany({ userId: user._id });
+  //   const salt = 10;
+  //   const hashedVerificationCode = await bcrypt.hash(verificationCode, salt);
 
-    const newPasswordReset = new PasswordReset({
-      userId: user._id,
-      verificationCode: hashedVerificationCode,
-      createdAt: Date.now(),
-      expiresAt: Date.now() + 3600,
-    });
+  //   const newPasswordReset = new PasswordReset({
+  //     userId: user._id,
+  //     verificationCode: hashedVerificationCode,
+  //     createdAt: Date.now(),
+  //     expiresAt: Date.now() + 3600,
+  //   });
 
-    await newPasswordReset.save();
-     transporter.sendMail(mailOptions, (error, info)=>{
-      if(error){
-        console.log(error);
-      }else{
-        console.log("Email has been sent Sucessfully" + info.response)
-      }
-    });
+  //   await newPasswordReset.save();
+  //   await transporter.sendMail(mailOptions, (error, info)=>{
+  //     if(error){
+  //       console.log(error);
+  //     }else{
+  //       console.log("Email has been sent Sucessfully" + info.response)
+  //     }
+  //   });
 
-  } catch (error) {
-    console.log(error.message);
-    throw error;
-  }
+  // } catch (error) {
+  //   console.log(error);
+  //   throw error;
+  // }
 };
 
 
