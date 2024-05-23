4ac215286956c0644fbeeb71329e14876e29dbb2
pious <109950629+pious2847@users.noreply.github.com>
2024-05-23 23:52:17 +0000

    Changes to be committed: 	modified:   server.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use((req, res, next) => {
   const start = Date.now();
   res.on('finish', () => {
       const duration = Date.now() - start;
-      console.log(`${req.method} ${req.path} took ${duration}ms`);
+      console.log(`${req.method} ${req.path} took ${duration}ms timestamp ${Date.now()}`);
   });
   next();
 });
