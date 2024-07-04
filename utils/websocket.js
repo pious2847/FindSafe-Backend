9c1d7828af0dba90a64060cf7d3a6f7fcd99efa0
pious <109950629+pious2847@users.noreply.github.com>
2024-07-04 23:01:00 +0000

    Changes to be committed: 	modified:   utils/websocket.js

--- utils/websocket.js
+++ utils/websocket.js
@@ -17,7 +17,7 @@ function startWebSocketServer(server) {
       if (data.command && data.deviceId) {
         console.log(`Sending command to ${data.deviceId}: ${data.command}`);
         try {
-          await clients[data.deviceId].send(message);
+          ws.send(message);
           console.log(`Command successfully sent to ${data.deviceId}`);
         } catch (error) {
           console.error(`Failed to send command to ${data.deviceId}: ${error.message}`);
