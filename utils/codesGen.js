16c4f9eb3fe448312c6542fcd6611a60a2ee7503
pious2847 <abdulhafis2847@gmail.com>
2025-05-23 11:50:55 +0000

    updates on generate otp function

--- utils/codesGen.js
+++ utils/codesGen.js
@@ -14,11 +14,18 @@ module.exports.capitalizeEachWord = function capitalizeEachWord(str) {
     return crypto.createHash('sha256').update(data).digest('hex');
   }
 
+  // Generate OTP for Verification Codes and Password Reset Codes.
+const generateOTP = () => {
+    return Math.floor(100000 + Math.random() * 900000); // Generate a random 6-digit OTP
+};
+
   // Generate JWT Token
 const generateToken = (user) => {
     return jwt.sign({ user }, process.env.JWT_SECRET || 'Secret_Key', {
         expiresIn: '7d',
     });
 };
+
+module.exports.generateOTP = generateOTP;
 
 module.exports.generateToken = generateToken;
